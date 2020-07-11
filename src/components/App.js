095bac3473cc038142a779525a332c73edb47b98
Daniel <dpericich@gmail.com>
2020-07-11 12:19:52 -0500

    Hooked up api to project Created basic page load to test api connection create css module to test way of styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,36 @@
 import React from 'react';
+import nasaApod from '../apis/nasaApod'
+import styles from './App.module.css'
 
 class App extends React.Component {
+
+    state={}
+    getNasaInformation = async() => {
+        let response = await nasaApod.get('');
+        this.setState(response.data)
+        console.log(this.state)
+    }
+
+    componentDidMount(){
+        this.getNasaInformation()
+    }
+
+
     render(){
         return(
-            <div style={backgroundStyle}>I am a nasa app!</div>
+            <div className={styles.siteBackground}>
+                <div>{this.state.title} | {this.state.date}</div>
+                <img alt={this.state.title} 
+                    src={this.state.url} 
+                    width="425px" 
+                    height="550px"
+                />
+                <figcaption>{this.state.explanation}</figcaption>
+            </div>
+
         )
     }
 }
 
-const backgroundStyle = {
-    backgroundColor: "black",
-    textAlign: 'center',
-    color: 'white'
-}
 
 export default App;
